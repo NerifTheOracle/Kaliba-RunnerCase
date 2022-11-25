28a7eee0a92f515198345531c889bf270322e7e5
NerifTheOracle <69296227+NerifTheOracle@users.noreply.github.com>
2022-11-25 23:12:45 +0300

    Small Fix

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
@@ -6,4 +7,17 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
    [SerializeField] private CinemachineVirtualCamera[] VirtualCameras;
+
+   private void Start()
+   {
+      GameManager.Instance.EventManager.Register(EventTypes.LevelStart,GameStarted);
+   }
+   void GameStarted(EventArgs args)
+   {
+      for (int i = 0; i < VirtualCameras.Length; i++)
+      {
+         VirtualCameras[i].gameObject.SetActive(false);
+      }
+      VirtualCameras[1].gameObject.SetActive(true);
+   }
 }
